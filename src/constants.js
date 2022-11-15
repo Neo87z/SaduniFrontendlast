809225351ab13bc3bc1e759f7d60acae3de00d50
Neo87z <shehanhoradagoda12@gmail.com>
2022-11-15 03:18:51 -0330

    first commit

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const PronounciationURL='http://03771e339781.ngrok.io'
+const EssayURL='http://a1e4-34-170-142-154.ngrok.io'
+const verbURl='http://eb48-34-125-22-1.ngrok.io'
+const GrammarURl='http://97e1-35-238-88-102.ngrok.io'
+export {PronounciationURL,EssayURL,verbURl,GrammarURl}
